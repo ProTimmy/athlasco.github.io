a18e96019ef3109d16595a491acc738788441d6b
Gabriel Timotei <gabtimotei@gmail.com>
2020-10-15 22:51:27 -0400

    Created TagEditor

---
+++ src/editor/Editor.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import TagEditor from './tags/TagEditor'
+
+class Editor extends React.Component {
+  render() {
+    return (
+      <TagEditor />
+    )
+  }
+}
+
+export default Editor;
---
+++ src/editor/tags/TagEditor.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class TagEditor extends React.Component {
+  render() {
+    return (
+      <div>
+        Tag Editor
+      </div>
+    )
+  }
+}
+
+export default TagEditor
