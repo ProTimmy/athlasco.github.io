dd03f05156524cc5c90cf3e385846c2003e878ad
Gabriel Timotei <gabtimotei@gmail.com>
2020-10-20 17:24:02 -0400

    Added Password Forget

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,6 +7,7 @@ import {
 import Navigation from '../Navigation'
 import LandingPage from '../Landing'
 import SignInPage from '../SignIn'
+import PasswordForgetPage from '../PasswordForget'
 import Editor from '../Editor'
 
 import * as ROUTES from '../../constants/routes'
@@ -20,6 +21,7 @@ const App = () => (
 
     <Route exact path={ROUTES.LANDING} component={LandingPage} />
     <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+    <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
     <Route path={ROUTES.EDITOR} component={Editor} />
   </Router>
 )
--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -1,38 +1,33 @@
 import React, { useState } from 'react'
-import { withRouter } from 'react-router-dom'
-import { compose } from 'recompose'
+import { Link } from 'react-router-dom'
 
-import { PasswordForgetLink } from '../PasswordForget'
 import { withFirebase } from '../Firebase'
 import * as ROUTES from '../../constants/routes'
 
-const SignInPage = () => (
+const PasswordForgetPage = () => (
   <div>
-    <h1>Sign In</h1>
-    <SignInForm />
-    <PasswordForgetLink />
+    <h1>Password Forget</h1>
+    <PasswordFogetForm />
   </div>
 )
 
 const INITIAL_STATE= {
-  email: '',
-  password: ''
+  email: ''
 }
 
-const SignInFormBase = (props) => {
+const PasswordForgetFormBase = (props) => {
   const [userInfo, setUserInfo] = useState(INITIAL_STATE)
   const [error, setError] = useState(null)
 
   const onSubmit = event => {
-    const { email, password } = userInfo
+    const { email } = userInfo
 
     props.firebase
-      .doSignInWithEmailAndPassword(email, password)
+      .doPasswordReset(email)
       .then(() => {
         setUserInfo(INITIAL_STATE)
-        props.history.push(ROUTES.EDITOR)
       })
-      .catch(error => {
+      .catch( error => {
         console.log(error)
         setError(error)
       })
@@ -45,7 +40,7 @@ const SignInFormBase = (props) => {
     setUserInfo((userInfo) => ({...userInfo, [name]: value}))
   }
 
-  const isInvalid = userInfo.password === '' || userInfo.email === ''
+  const isInvalid = userInfo.email === ''
 
   return (
     <form onSubmit={onSubmit}>
@@ -56,15 +51,8 @@ const SignInFormBase = (props) => {
         type="text"
         placeholder="Email Address"
       />
-      <input
-        name="password"
-        value={userInfo.password}
-        onChange={onChange}
-        type="password"
-        placeholder="Password"
-      />
       <button disabled={isInvalid} type="submit">
-        Sign In
+        Reset My Password
       </button>
 
       {error && <p>{error.message}</p>}
@@ -72,11 +60,14 @@ const SignInFormBase = (props) => {
   )
 }
 
-const SignInForm = compose(
-  withRouter,
-  withFirebase
-) (SignInFormBase)
+const PasswordForgetLink = () => (
+  <p>
+    <Link to={ROUTES.PASSWORD_FORGET}>Forgot Password?</Link>
+  </p>
+)
 
-export default SignInPage
+export default PasswordForgetPage
 
-export { SignInForm }
+const PasswordFogetForm = withFirebase(PasswordForgetFormBase)
+
+export { PasswordFogetForm, PasswordForgetLink }
