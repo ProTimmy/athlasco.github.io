72e4866e2e7e80f01484eb0ff31a34748300579a
Gabriel Timotei <gabtimotei@gmail.com>
2020-12-29 16:19:53 -0500

    Modified eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   extends: ['airbnb-base', 'plugin:react/recommended'],
   parser: 'babel-eslint',
+  rules: {
+    'no-use-before-define': ['error', { variables: false }],
+  },
   settings: {
     'import/resolver': {
       node: {
